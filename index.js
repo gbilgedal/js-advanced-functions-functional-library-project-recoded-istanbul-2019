b7a213a20df96eabbf4aa53066ba04a921d715ea
G Bilge Dal <lagala999@gmail.com>
2020-05-04 14:18:13 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,31 +4,12 @@ const fi = (function() {
       return 'Start by reading https://medium.com/javascript-scene/master-the-javascript-interview-what-is-functional-programming-7f218c68b3a0'
     },
 
-    each: function(collection, callback) {
-      let orj = collection;
-      if(typeof collection[0] == "object"){
-        collection.forEach((value, key, callback) => callback(value, key, collection));
-        return orj;
-      }
-      else{
-        collection.forEach((el, index, callback) => callback(el, index, collection));
-        return orj;
-      }
+    each: function() {
+
     },
 
-    map: function(collection, callback) {
-      if(typeof collection[0] == "object"){
-        let newCollection = collection.map(el=>{
-          return callback(value,key,callback);
-        });
-      }
-      else{
-        let newCollection = collection.map(el=>{
-          return callback(value,index,callback);
-        });
-      }
+    map: function() {
 
-      return newCollection
     },
 
     reduce: function() {
