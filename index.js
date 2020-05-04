08441e8b337eec71b9627e80d8fe06c980efc649
G Bilge Dal <lagala999@gmail.com>
2020-05-04 14:00:42 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,7 +4,16 @@ const fi = (function() {
       return 'Start by reading https://medium.com/javascript-scene/master-the-javascript-interview-what-is-functional-programming-7f218c68b3a0'
     },
 
-    each: function() {
+    each: function(collection, callback) {
+      if(typeofof collection[0] === number){
+        collection.forEach((el, index) => callback(value, key, collection));
+        return collection;
+      }
+      else{
+        collection.forEach((el, index) => callback(el, index, collection));
+        return collection;
+      }
+      
 
     },
 
